f01e69f93da431179623ab324536045c51155349
Nicholas Yoong <nicyoong321@gmail.com>
2025-03-30 23:36:45 +0800

    print message function

--- philo.c
+++ philo.c
@@ -6,15 +6,20 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 23:35:00 by nyoong            #+#    #+#             */
-/*   Updated: 2025/03/30 23:35:17 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/03/30 23:36:36 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 unsigned long get_current_time() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
+void print_message(t_philosopher *philo, const char *msg) {
+	pthread_mutex_lock(philo->printf_mutex);
+	printf("%lu %d %s\n", get_current_time(), philo->id, msg);
+	pthread_mutex_unlock(philo->printf_mutex);
+}
