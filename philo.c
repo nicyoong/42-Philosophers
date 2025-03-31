d3bef49ea9f7975ddb4a112ec66433f219ca3074
Nicholas Yoong <nicyoong321@gmail.com>
2025-03-31 19:29:40 +0800

    add ft atoi

--- philo.c
+++ philo.c
@@ -6,11 +6,36 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 23:35:00 by nyoong            #+#    #+#             */
-/*   Updated: 2025/03/31 19:28:56 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/03/31 19:29:32 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		else
+			sign = 1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result * sign);
+}
 
 unsigned long	get_current_time()
 {
