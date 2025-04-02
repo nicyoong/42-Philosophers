a9e52acb02ce4422422aeb776eeadfaced388299
Nicholas Yoong <nicyoong321@gmail.com>
2025-04-03 00:13:28 +0800

    chore: moving function
    moving check_philosopher_status to philomonitor.c

--- philo/philo.c
+++ philo/philo.c
@@ -6,25 +6,11 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 23:35:00 by nyoong            #+#    #+#             */
-/*   Updated: 2025/04/03 00:10:27 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/04/03 00:13:03 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-bool check_philosopher_status(t_philosopher *philo)
-{
-	bool			starving;
-	unsigned long	time_since_meal;
-	unsigned long	current_time;
-
-	current_time = get_current_time();
-	pthread_mutex_lock(&philo->meal_mutex);
-	time_since_meal = current_time - philo->last_meal_time;
-	starving = (time_since_meal > philo->time_to_die);
-	pthread_mutex_unlock(&philo->meal_mutex);
-	return (starving);
-}
 
 bool	check_meal_completion(t_philosopher *philos,
 	int num_philos, int required)
--- philo/philomonitor.c
+++ philo/philomonitor.c
@@ -6,7 +6,7 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 00:10:11 by nyoong            #+#    #+#             */
-/*   Updated: 2025/04/03 00:10:33 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/04/03 00:13:10 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,18 @@ void	handle_philosopher_death(t_philosopher *philo)
 	pthread_mutex_unlock(philo->printf_mutex);
 	pthread_mutex_unlock(&philo->meal_mutex);
 	exit(EXIT_SUCCESS);
+}
+
+bool check_philosopher_status(t_philosopher *philo)
+{
+	bool			starving;
+	unsigned long	time_since_meal;
+	unsigned long	current_time;
+
+	current_time = get_current_time();
+	pthread_mutex_lock(&philo->meal_mutex);
+	time_since_meal = current_time - philo->last_meal_time;
+	starving = (time_since_meal > philo->time_to_die);
+	pthread_mutex_unlock(&philo->meal_mutex);
+	return (starving);
 }
