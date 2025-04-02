8f9963fe9a380a0913aca19f565aac9b8e53e923
Nicholas Yoong <nicyoong321@gmail.com>
2025-04-03 00:03:35 +0800

    chore: moving take forks function to philolife.c

--- philo/philo.c
+++ philo/philo.c
@@ -6,30 +6,11 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 23:35:00 by nyoong            #+#    #+#             */
-/*   Updated: 2025/04/03 00:02:51 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/04/03 00:03:09 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void    take_forks(t_philosopher *philo)
-{
-	if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(philo->left_fork);
-		print_message(philo, "has taken a fork");
-		pthread_mutex_lock(philo->right_fork);
-		print_message(philo, "has taken a fork");
-	}
-	else
-	{
-		pthread_mutex_lock(philo->right_fork);
-		print_message(philo, "has taken a fork");
-		precise_usleep(100);
-		pthread_mutex_lock(philo->left_fork);
-		print_message(philo, "has taken a fork");
-	}
-}
 
 void	update_last_meal(t_philosopher *philo)
 {
--- philo/philolife.c
+++ philo/philolife.c
@@ -6,7 +6,7 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 00:01:55 by nyoong            #+#    #+#             */
-/*   Updated: 2025/04/03 00:02:20 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/04/03 00:03:16 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,23 @@
 void	think(t_philosopher *philo)
 {
 	print_message(philo, "is thinking");
+}
+
+void    take_forks(t_philosopher *philo)
+{
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		print_message(philo, "has taken a fork");
+		pthread_mutex_lock(philo->right_fork);
+		print_message(philo, "has taken a fork");
+	}
+	else
+	{
+		pthread_mutex_lock(philo->right_fork);
+		print_message(philo, "has taken a fork");
+		precise_usleep(100);
+		pthread_mutex_lock(philo->left_fork);
+		print_message(philo, "has taken a fork");
+	}
 }
