db37b91041d255e2bfc2d488a82393cfe7199846
Nicholas Yoong <nicyoong321@gmail.com>
2025-04-03 00:10:49 +0800

    chore: moving handle philosopher death to philomonitor.c

--- philo/philo.c
+++ philo/philo.c
@@ -6,21 +6,11 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 23:35:00 by nyoong            #+#    #+#             */
-/*   Updated: 2025/04/03 00:09:06 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/04/03 00:10:27 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void	handle_philosopher_death(t_philosopher *philo)
-{
-	pthread_mutex_lock(&philo->meal_mutex);
-	pthread_mutex_lock(philo->printf_mutex);
-	printf("%lu %d died\n", get_current_time() / 1000, philo->id);
-	pthread_mutex_unlock(philo->printf_mutex);
-	pthread_mutex_unlock(&philo->meal_mutex);
-	exit(EXIT_SUCCESS);
-}
 
 bool check_philosopher_status(t_philosopher *philo)
 {
