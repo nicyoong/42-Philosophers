bfee40a04924c59a709fc2522eeeba40cfb159bf
Nicholas Yoong <nicyoong321@gmail.com>
2025-04-02 23:50:29 +0800

    chore: removing ft atoi
    ft atoi moved to philoutils.c

--- philo/philo.c
+++ philo/philo.c
@@ -6,36 +6,11 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 23:35:00 by nyoong            #+#    #+#             */
-/*   Updated: 2025/04/02 23:43:04 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/04/02 23:47:18 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-int	ft_atoi(const char *str)
-{
-	int	sign;
-	int	result;
-
-	sign = 1;
-	result = 0;
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
-		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = -1;
-		else
-			sign = 1;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		result = result * 10 + (*str - '0');
-		str++;
-	}
-	return (result * sign);
-}
 
 unsigned long	get_current_time()
 {
